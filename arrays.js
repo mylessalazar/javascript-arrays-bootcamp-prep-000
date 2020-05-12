25d4b33c6e98073bae32c09b6e9906b9bc440fdf
Myles <mylesssalazar@gmail.com>
2020-05-12 00:54:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,3 +33,6 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
   chocolateBars.pop();
   return chocolateBars;
 }
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length -1);
+}
